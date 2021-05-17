2238062e0cc9ce0f4fc383730e5bf9cb9db3a972
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-16 22:18:38 -0300

    classe room criada e finalizado Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Room;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Room[] vect = new Room[10];
+		
+		System.out.println("How many rooms will be rented? ");
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++) {
+			System.out.printf("%nRent #%d:%n", i+1);
+			System.out.println("name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Email: ");
+			String email = sc.nextLine();
+			System.out.println("Room: ");
+			int roomNumber = sc.nextInt();
+			vect[roomNumber] = new Room(name, email, roomNumber);
+		}
+		
+		System.out.println("\nBusy rooms:\n");
+		for (int i=0; i<vect.length; i++) {
+			if (vect[i] != null) {
+				//System.out.printf("%d: %s, %s%n", vect[i].getRoomNumber(), vect[i].getName(), vect[i].getEmail());
+				System.out.println(vect[i]);
+			}
+		}
+		
+		sc.close();
 	}
 
 }
